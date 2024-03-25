64e4204a2e3147d99afd14d49f78a7e3b5dd08b2
Mueez Nadeem <70438888+MueezNadeem@users.noreply.github.com>
2024-03-25 21:33:59 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { Week1 } from './components/Week1/Week1';
+
+interface Props {
+  className?: string;
+}
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+    <div className={`${resets.storybrainResets} ${classes.root}`}>
+      <Week1 />
+    </div>
+  );
+});
